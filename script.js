6e65450573d39e45162c7342f93ed6a123d9c7eb
arsam109 <alarmtechniques@gmail.com>
2026-06-15 08:39:21 +0300

    Update script.js

--- script.js
+++ script.js
@@ -119,3 +119,67 @@ card.style.transform = "";
 
 });
 
+const canvas = document.getElementById("space");
+const ctx = canvas.getContext("2d");
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+const stars = [];
+
+for(let i = 0; i < 250; i++){
+
+stars.push({
+
+x: Math.random() * canvas.width,
+y: Math.random() * canvas.height,
+size: Math.random() * 2,
+speed: Math.random() * 0.5 + 0.1
+
+});
+
+}
+
+function animateStars(){
+
+ctx.clearRect(0,0,canvas.width,canvas.height);
+
+ctx.fillStyle = "white";
+
+stars.forEach(star=>{
+
+ctx.beginPath();
+
+ctx.arc(
+star.x,
+star.y,
+star.size,
+0,
+Math.PI*2
+);
+
+ctx.fill();
+
+star.y += star.speed;
+
+if(star.y > canvas.height){
+
+star.y = 0;
+star.x = Math.random() * canvas.width;
+
+}
+
+});
+
+requestAnimationFrame(animateStars);
+
+}
+
+animateStars();
+
+window.addEventListener("resize",()=>{
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+});
