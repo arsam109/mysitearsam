d86332574b85552d973aa51cd503b54a4ea2a29b
arsam109 <alarmtechniques@gmail.com>
2026-06-15 08:24:47 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,47 @@
+const texts = [
+"Full Stack Developer",
+"Future Pilot",
+"Game Creator",
+"Problem Solver"
+];
 
+let i = 0;
+
+const typing = document.getElementById("typing");
+
+setInterval(() => {
+typing.textContent = texts[i];
+i++;
+if(i >= texts.length) i = 0;
+},2000);
+
+const counters =
+document.querySelectorAll(".counter");
+
+counters.forEach(counter=>{
+
+const target =
++counter.dataset.target;
+
+let count = 0;
+
+const update = ()=>{
+
+count += Math.ceil(target/100);
+
+if(count >= target){
+
+counter.innerText = target;
+return;
+
+}
+
+counter.innerText = count;
+
+requestAnimationFrame(update);
+
+};
+
+update();
+
+});
